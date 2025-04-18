a200e109062324a50ce608662a52793751bd7cf8
dadah <iwolofar@yahoo.com>
2025-04-18 13:50:50 -0400

    adding jenkinsfile

--- summary.py
+++ summary.py
@@ -1,3 +1,8 @@
-Summary Page
-Full of some critical information
+team1 = "ManUtd"
+team2 = "Arsenal"
 
+team1_score = 4
+team2_score = 2
+
+
+print(f'The final score is: {team1} {team1_score} vs {team2} {team2_score}')
